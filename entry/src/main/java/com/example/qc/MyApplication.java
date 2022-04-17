0bdc814314fd6842d0e1f54c787f54f1b49662dc
李毅\liyi <1317208044@qq.com>
2022-04-17 20:32:52 +0800

    add

---
+++ entry/src/main/java/com/example/qc/MyApplication.java
@@ -0,0 +1,10 @@
+package com.example.qc;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
---
+++ entry/src/test/java/com/example/qc/ExampleTest.java
@@ -0,0 +1,9 @@
+package com.example.qc;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void onStart() {
+    }
+}
